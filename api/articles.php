7fbf002e06c83205e4c7860da044a96b3939af76
Alexandr <saniabublik5@gmail.com>
2026-02-23 18:08:18 +0300

    feat: добавить расширенный функционал к получению статьи

--- api/articles.php
+++ api/articles.php
@@ -57,3 +57,34 @@ function getArticles($connect, $topic_id = null) {
 
     }
 }
+
+function getArticle($connect, $artcle_id = null) {
+    if (!$artcle_id) {
+        http_response_code(404);
+        return json_encode([
+            "status" => false,
+            "message" => "статья не найдена"
+        ]);
+    }
+
+    $artcle_id = (int)$artcle_id;
+    $query = mysqli_query($connect, "SELECT articles.*, modules.title AS module_title FROM articles LEFT JOIN topics ON articles.topic_id = topics.id LEFT JOIN modules ON topics.module_id = modules.id WHERE articles.id = $artcle_id");
+        
+    if (!$query || mysqli_num_rows($query) === 0) {
+        http_response_code(404);
+        return json_encode([
+            "status" => false,
+            "message" => "Статья не найдена"
+            ]);
+    }
+        
+    $articles = mysqli_fetch_assoc($query);
+        
+    http_response_code(200);
+    return json_encode([
+        "status" => true,
+        "message" => "Статьи найдены",
+        "data" => $articles
+    ], JSON_UNESCAPED_UNICODE);
+    
+}
